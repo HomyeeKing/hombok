f67e0c10ba53f0b77b1750d26e41b19974fea22a
HomyeeKing <HomyeeKing@gmail.com>
2021-08-28 17:26:22 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <h1 class="text-blue-800 underline text-2xl italic">HOMBOK!</h1>
+  <small class="capitalize ml-70">—— the intelligence of homyee king </small>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
